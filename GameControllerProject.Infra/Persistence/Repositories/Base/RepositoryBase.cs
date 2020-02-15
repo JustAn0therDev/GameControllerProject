c3dae0bc11f11a5143a6bd3ebad83a1d5dd0bb63
Ruan Montelo <ruanmontelo@gmail.com>
2020-02-15 15:09:46 -0300

    Finalizando implementação de classe de RepositoryBase

--- GameControllerProject.Infra/Persistence/Repositories/Base/RepositoryBase.cs
+++ GameControllerProject.Infra/Persistence/Repositories/Base/RepositoryBase.cs
@@ -8,8 +8,8 @@ using System.Linq.Expressions;
 
 namespace GameControllerProject.Infra.Persistence.Repositories.Base
 {
-    public class RepositoryBase<TEntity, TId> : IRepositoryBase<TEntity, TId> 
-        where TEntity : EntityBase 
+    public class RepositoryBase<TEntity, TId> : IRepositoryBase<TEntity, TId>
+        where TEntity : EntityBase
         where TId : struct
     {
         protected readonly DbContext _context;
@@ -45,32 +45,65 @@ namespace GameControllerProject.Infra.Persistence.Repositories.Base
 
         public IQueryable<TEntity> GetAll()
         {
-            throw new NotImplementedException();
+            return _context.Set<TEntity>().Select(s => s);
+        }
+
+        public IQueryable<TEntity> Get(params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            IQueryable<TEntity> query = _context.Set<TEntity>();
+
+            if (includeProperties.Any())
+            {
+                return Include(_context.Set<TEntity>(), includeProperties);
+            }
+
+            return query;
         }
 
         public IQueryable<TEntity> GetAndOrderBy<TKey>(Expression<Func<TEntity, bool>> where, Expression<Func<TEntity, TKey>> ordem, bool ascendente = true, params Expression<Func<TEntity, object>>[] includeProperties)
         {
-            throw new NotImplementedException();
+            return ascendente ? Get(includeProperties).OrderBy(ordem) : Get(includeProperties).OrderByDescending(ordem);
         }
 
-        public IQueryable<TEntity> GetBy(Expression<Func<TEntity, bool>> where, params Expression<Func<TEntity, object>>[] includeProperties)
+        public IQueryable<TEntity> GetBy(Expression<Func<TEntity, bool>> expression, params Expression<Func<TEntity, object>>[] includeProperties)
         {
-            throw new NotImplementedException();
+            return Get(includeProperties).Where(expression);
         }
 
         public TEntity GetById(TId id)
         {
-            throw new NotImplementedException();
+            return _context.Set<TEntity>().Find(id);
         }
 
         public IQueryable<TEntity> GetOrderedBy<TKey>(Expression<Func<TEntity, TKey>> order, bool ascending)
         {
-            throw new NotImplementedException();
+            return ascending ? _context.Set<TEntity>().OrderBy(order) : _context.Set<TEntity>().OrderByDescending(order);
         }
 
         public TEntity Update(TEntity entity)
         {
-            throw new NotImplementedException();
+            using (var result = _context.Set<TEntity>().Find(entity.Id))
+            {
+                if (result != null)
+                    _context.SaveChanges();
+            }
+            return entity;
+        }
+
+        /// <summary>
+        /// Realiza include populando o objeto passado por parametro
+        /// </summary>
+        /// <param name="query">Informe o objeto do tipo IQuerable</param>
+        /// <param name="includeProperties">Ínforme o array de expressões que deseja incluir</param>
+        /// <returns></returns>
+        private IQueryable<TEntity> Include(IQueryable<TEntity> query, params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            foreach (var property in includeProperties)
+            {
+                query = query.Include(property);
+            }
+
+            return query;
         }
     }
 }
