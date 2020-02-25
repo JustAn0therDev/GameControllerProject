102052e2579148c1ae646006f357ca5703a4b0aa
Ruan Montelo <ruanmontelo@gmail.com>
2020-02-25 14:46:35 -0300

    Correção na tabela de Platform na Migration atual para comportar criação da GamePlatform.

---
+++ GameControllerProject.Infra/Migrations/202002251736495_UpdatingDatabaseGamePlatform.cs
@@ -0,0 +1,27 @@
+﻿namespace GameControllerProject.Infra.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdatingDatabaseGamePlatform : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Platform", "Game_Id", "dbo.Game");
+            DropIndex("dbo.Platform", new[] { "Game_Id" });
+            DropColumn("dbo.Platform", "Game_Id");
+
+            //DropForeignKey("dbo.Platform", "Game_Id1", "dbo.Game");
+            //DropIndex("dbo.Platform", new[] { "Game_Id1" });
+            //DropColumn("dbo.Platform", "Game_Id1");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Platform", "Game_Id1", c => c.Guid());
+            AddColumn("dbo.Platform", "Game_Id", c => c.Guid());
+            CreateIndex("dbo.Platform", "Game_Id1");
+            AddForeignKey("dbo.Platform", "Game_Id1", "dbo.Game", "Id");
+        }
+    }
+}
